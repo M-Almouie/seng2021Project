a6dfde757b401d31fe64333058f2ccc81c88d20e
Mohamed Al Mouiee <m.almouiee@student.unsw.edu.au>
2017-09-20 20:07:09 +1000

    Current user location added to the map

--- source/map.js
+++ source/map.js
@@ -1,10 +1,11 @@
 function initMap() {
     var sydney = {lat: -33.8688, lng: 151.2093};
+    var unsw = {lat: -33.9173, lng: 151.2313};
     var tartous = {lat: 34.8959, lng: 35.8867};
     var roseBay = {lat: -33.8754, lng: 151.2656};
     var map1 = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
-        center: sydney
+        center: unsw
     });
     var map2 = new google.maps.Map(document.getElementById('mapSecond'), {
         zoom: 13,
@@ -12,6 +13,11 @@ function initMap() {
     });
 
     var friendImage = "img/happy.png";
+    var me = new google.maps.Marker({
+        position: unsw,
+        map: map1,
+        animation: google.maps.Animation.DROP,
+    });
     var marker1 = new google.maps.Marker({
         position: sydney,
         map: map1,
@@ -30,6 +36,18 @@ function initMap() {
         icon: friendImage,
         animation: google.maps.Animation.Drop,
     });
+
+    var meString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">You Are Here</h1>'+
+        '<div id="bodyContent">'+
+        '<p>Mohamed Al Mouiee</p>'+
+        '<p>Location: Randwick, Australia</p>'+
+        '<p>Currently Active</p>'+
+        '</div>'+
+        '</div>';
+
     var moString = '<div id="content">'+
         '<div id="siteNotice">'+
         '</div>'+
@@ -37,7 +55,7 @@ function initMap() {
         '<div id="bodyContent">'+
         '<p>Mohamed Al Mouiee</p>'+
         '<p>Location: Sydney</p>'+
-        '<p>Last Senn: 12:39pm 19/09/2017</p>'+
+        '<p>Last Seen: 12:39pm 19/09/2017</p>'+
         '</div>'+
         '</div>';
 
@@ -52,11 +70,19 @@ function initMap() {
         '</div>'+
         '</div>';
 
+    var infowindow0 = new google.maps.InfoWindow({
+        content: meString
+    });
+
     var infowindow1 = new google.maps.InfoWindow({
         content: moString
     });
     var infowindow2 = new google.maps.InfoWindow({
         content: joString
+    });
+
+    me.addListener('click', function() {
+        infowindow0.open(map1, me);
     });
 
     marker1.addListener('click', function() {
@@ -65,6 +91,14 @@ function initMap() {
     marker2.addListener('click', function() {
         infowindow2.open(map1, marker2);
     });
+
+    function toggleBounce0() {
+        if (me.getAnimation() !== null) {
+            me.setAnimation(null);
+        } else {
+            me.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
 
     function toggleBounce1() {
         if (marker1.getAnimation() !== null) {
@@ -87,6 +121,8 @@ function initMap() {
             marker3.setAnimation(google.maps.Animation.BOUNCE);
         }
     }
+
+    me.addListener('mousemove', toggleBounce0);
     marker1.addListener('click', toggleBounce1);
     marker2.addListener('click', toggleBounce2);
     marker3.addListener('click', toggleBounce3);
