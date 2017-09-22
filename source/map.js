b18187e3799d31b3a89f573e651419f5d7c5cfd2
Mohamed Al Mouiee <m.almouiee@student.unsw.edu.au>
2017-09-22 12:14:12 +1000

    Fixed map.js

--- source/map.js
+++ source/map.js
@@ -1,4 +1,56 @@
 function initMap() {
+
+    function createMap(ID, zoom, center) {
+        var map = new google.maps.Map(document.getElementById(ID), {
+            zoom : zoom,
+            center: center
+        });
+        return map;
+    }
+
+    function createFriendMarker(position, map, icon) {
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            icon: icon,
+            animation: google.maps.Animation.DROP,
+        });
+        return marker;
+    }
+
+    function createUserMarker(position, map) {
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            animation: google.maps.Animation.DROP,
+        });
+        return marker;
+    }
+
+    function WindowInfo(str) {
+        var infowindow = new google.maps.InfoWindow({
+            content: str
+        });
+        return infowindow;
+    }
+
+
+    function createPopupBox(map, marker, infowindow) {
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+    }
+
+    function startBounce(marker) {
+        marker.addListener('mousemove', function tooggleBounce() {
+            if (marker.getAnimation() !== null) {
+                marker.setAnimation(null);
+            } else {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+            }
+        });
+    }
+
     var sydney = {lat: -33.8688, lng: 151.2093};
     var unsw = {lat: -33.9173, lng: 151.2313};
     var tartous = {lat: 34.8959, lng: 35.8867};
@@ -7,60 +59,20 @@ function initMap() {
     var unswQuadrangle = {lat:-33.916999, lng:151.230279};
     var cse = {lat  : -33.918628, lng: 151.231132};
     var coogee = {lat:-33.919981, lng:151.258340};
-    var map1 = new google.maps.Map(document.getElementById('map'), {
-        zoom: 17,
-        center: unswQuadrangle
-    });
-    var map2 = new google.maps.Map(document.getElementById('mapSecond'), {
-        zoom: 17,
-        center: unswQuadrangle
-    });
+
+    var map1 = createMap('map',17,unswQuadrangle);
+    var map2 = createMap('mapSecond',17,unswQuadrangle);
 
     var friendImage = "img/MyTrackLogo.png";
-    var me = new google.maps.Marker({
-        position: unswQuadrangle,
-        map: map1,
-        animation: google.maps.Animation.DROP,
-    });
-    var marker1 = new google.maps.Marker({
-        position: tartous,
-        map: map1,
-        icon: friendImage,
-        animation: google.maps.Animation.DROP,
-    });
-    var marker2 = new google.maps.Marker({
-        position: roseBay,
-        map: map1,
-        icon: friendImage,
-        animation: google.maps.Animation.Drop,
-    });
-    var marker3 = new google.maps.Marker({
-        position: unswLibrary,
-        map: map1,
-        icon: friendImage,
-        animation: google.maps.Animation.Drop,
-    });
 
-    var marker4 = new google.maps.Marker({
-        position: cse,
-        map: map1,
-        icon: friendImage,
-        animation: google.maps.Animation.Drop,
-    });
+    var me = createUserMarker(unswQuadrangle,map1);
+    var marker1 = createFriendMarker(tartous, map1, friendImage);
+    var marker2 = createFriendMarker(roseBay, map1, friendImage);
+    var marker3 = createFriendMarker(unswLibrary, map1, friendImage);
+    var marker4 = createFriendMarker(cse, map1, friendImage);
+    var marker5 = createFriendMarker(sydney, map1, friendImage);
+    var marker6 = createFriendMarker(coogee, map1, friendImage);
 
-    var marker5 = new google.maps.Marker({
-        position: sydney,
-        map: map1,
-        icon: friendImage,
-        animation: google.maps.Animation.Drop,
-    });
-
-    var marker6 = new google.maps.Marker({
-        position: coogee,
-        map: map1,
-        icon: friendImage,
-        animation: google.maps.Animation.Drop,
-    });
     var meString = '<div id="content">'+
         '<div id="siteNotice">'+
         '</div>'+
@@ -69,17 +81,6 @@ function initMap() {
         '<p>Mohamed Al Mouiee</p>'+
         '<p>Location: Randwick, Australia</p>'+
         '<p>Currently Active</p>'+
-        '</div>'+
-        '</div>';
-
-    var moString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">No body</h1>'+
-        '<div id="bodyContent">'+
-        '<p>No Body</p>'+
-        '<p>Location: tartous</p>'+
-        '<p>Last Seen: 12:39pm 19/09/2017</p>'+
         '</div>'+
         '</div>';
 
@@ -116,6 +117,17 @@ function initMap() {
         '</div>'+
         '</div>';
 
+    var moString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Every body</h1>'+
+        '<div id="bodyContent">'+
+        '<p>Every body</p>'+
+        '<p>Location: Randwick, Australia</p>'+
+        '<p>Last Seen: 12:39pm 19/09/2017</p>'+
+        '</div>'+
+        '</div>';
+
     var doString = '<div id="content">'+
         '<div id="siteNotice">'+
         '</div>'+
@@ -138,119 +150,32 @@ function initMap() {
         '</div>'+
         '</div>';
 
-    var infowindow0 = new google.maps.InfoWindow({
-        content: meString
-    });
+    var infowindow0 = WindowInfo(meString);
+    createPopupBox(map1,me,infowindow0);
 
-    var infowindow1 = new google.maps.InfoWindow({
-        content: moString
-    });
-    var infowindow2 = new google.maps.InfoWindow({
-        content: joString
-    });
+    var infowindow1 = WindowInfo(moString);
+    createPopupBox(map1,marker1,infowindow1);
 
-    var infowindow3 = new google.maps.InfoWindow({
-        content: doString
-    });
+    var infowindow2 = WindowInfo(joString);
+    createPopupBox(map1,marker2,infowindow2);
 
-    var infowindow4 = new google.maps.InfoWindow({
-        content: soString
-    });
+    var infowindow3 = WindowInfo(doString);
+    createPopupBox(map1,marker3,infowindow3);
 
-    var infowindow5 = new google.maps.InfoWindow({
-        content: foString
-    });
+    var infowindow4 = WindowInfo(soString);
+    createPopupBox(map1,marker4,infowindow4);
 
-    var infowindow6 = new google.maps.InfoWindow({
-        content: boString
-    });
+    var infowindow5 = WindowInfo(foString);
+    createPopupBox(map1,marker5,infowindow5);
 
-    me.addListener('click', function() {
-        infowindow0.open(map1, me);
-    });
+    var infowindow6 = WindowInfo(boString);
+    createPopupBox(map1,marker6,infowindow6);
 
-    marker1.addListener('click', function() {
-        infowindow1.open(map1, marker1);
-    });
-
-    marker2.addListener('click', function() {
-        infowindow2.open(map1, marker2);
-    });
-
-    marker3.addListener('click', function() {
-        infowindow3.open(map1, marker3);
-    });
-
-    marker4.addListener('click', function() {
-        infowindow4.open(map1, marker4);
-    });
-
-    marker5.addListener('click', function() {
-        infowindow5.open(map1, marker5);
-    });
-
-    marker6.addListener('click', function() {
-        infowindow6.open(map1, marker6);
-    });
-    function toggleBounce0() {
-        if (me.getAnimation() !== null) {
-            me.setAnimation(null);
-        } else {
-            me.setAnimation(google.maps.Animation.BOUNCE);
-        }
-    }
-
-    function toggleBounce1() {
-        if (marker1.getAnimation() !== null) {
-            marker1.setAnimation(null);
-        } else {
-            marker1.setAnimation(google.maps.Animation.BOUNCE);
-        }
-    }
-    function toggleBounce2() {
-        if (marker2.getAnimation() !== null) {
-            marker2.setAnimation(null);
-        } else {
-            marker2.setAnimation(google.maps.Animation.BOUNCE);
-        }
-    }
-    function toggleBounce3() {
-        if (marker3.getAnimation() !== null) {
-            marker3.setAnimation(null);
-        } else {
-            marker3.setAnimation(google.maps.Animation.BOUNCE);
-        }
-    }
-
-    function toggleBounce4() {
-        if (marker4.getAnimation() !== null) {
-            marker4.setAnimation(null);
-        } else {
-            marker4.setAnimation(google.maps.Animation.BOUNCE);
-        }
-    }
-
-    function toggleBounce5() {
-        if (marker5.getAnimation() !== null) {
-            marker5.setAnimation(null);
-        } else {
-            marker5.setAnimation(google.maps.Animation.BOUNCE);
-        }
-    }
-
-    function toggleBounce6() {
-        if (marker6.getAnimation() !== null) {
-            marker6.setAnimation(null);
-        } else {
-            marker6.setAnimation(google.maps.Animation.BOUNCE);
-        }
-    }
-
-    me.addListener('mousemove', toggleBounce0);
-    marker1.addListener('mousemove', toggleBounce1);
-    marker2.addListener('mousemove', toggleBounce2);
-    marker3.addListener('mousemove', toggleBounce3);
-    marker4.addListener('mousemove', toggleBounce4);
-    marker5.addListener('mousemove', toggleBounce5);
-    marker6.addListener('mousemove', toggleBounce6);
+    startBounce(me);
+    startBounce(marker1);
+    startBounce(marker2);
+    startBounce(marker3);
+    startBounce(marker4);
+    startBounce(marker5);
+    startBounce(marker6);
 }
